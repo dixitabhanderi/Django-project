2234e24b1247c61a000f7b94aa708af15069d227
Dixita <dixitabhanderi101@gmail.com>
2023-08-10 16:29:46 -0700

    update operation edit

--- notes/views.py
+++ notes/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import UpdateView, CreateView, DetailView, ListView
 
 from .forms import NotesForm
 from .models import Notes
 # views
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
 
 class NotesCreateView(CreateView):
     model = Notes
@@ -19,3 +24,4 @@ class NotesListView(ListView):
 class NotesDetailView(DetailView):
     model = Notes
     context_object_name = "note"
+    template_name = 'notes/notes_detail.html'
